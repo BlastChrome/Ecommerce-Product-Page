167059507db84eb0f06e8c9ff459df4f2d1a0fa2
Nicholas Jackson <nick@Nicholass-MacBook-Pro.local>
2023-11-27 09:04:44 -0800

    refactored my nav.js to be more generic, instead it's the active-manager, controls the state of which elements are active at a given time

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
 import "./scss/main.scss";
-import MobileNav from "./modules/mobile-nav";
+import ActiveManager from "./modules/active-manager";
 import Slider from "./modules/slider";
-
 const App = (() => {
-  const mobile_nav = new MobileNav();
-  const slide_1 = new Slider(".featured-product__slider--big");
-  // slide_1.slide({
-  //   slidesToShow: 1,
-  //   speed: 300,
-  //   autoPlaySpeed: 500,
-  //   infinite: true,
-  // });
+  const active_manager = new ActiveManager();
 })();
---
+++ src/modules/active-manager.js
@@ -0,0 +1,57 @@
+export default class ActiveManager {
+  constructor() {
+    this.cacheDom();
+    this.initEvents();
+    this.activeElements = [];
+  }
+
+  cacheDom = () => {
+    this.hamburger = document.getElementById("hamburger");
+    this.nav = document.getElementById("nav");
+    this.overlay = document.getElementById("overlay");
+  };
+
+  initEvents = () => {
+    this.hamburger.addEventListener("click", this.handleHamburgerClick);
+    this.overlay.addEventListener("click", this.handleOverlayClick);
+  };
+
+  toggle = (elements) => {
+    //toggle the active state of the passed in elements
+    elements.forEach((item) => {
+      item.classList.toggle("active");
+      //if those elements have been toggled to 'active' add them to the list of currently active elements
+      if (
+        item.classList.contains("active") &&
+        !this.activeElements.includes(item)
+      ) {
+        this.addToActive(item);
+      } else {
+        this.removeActive(item);
+      }
+    });
+    console.log(this.activeElements);
+  };
+
+  addToActive = (item) => {
+    this.activeElements.push(item);
+  };
+
+  removeActive = (element) => {
+    this.activeElements = this.activeElements.filter((item) => item != element);
+  };
+
+  handleHamburgerClick = (e) => {
+    e.stopPropagation();
+    this.toggle([this.hamburger, this.nav, this.overlay]);
+  };
+
+  handleOverlayClick = (e) => {
+    if (e.target == this.overlay && this.overlay.classList.contains("active")) {
+      this.activeElements.forEach((item) => {
+        item.classList.remove("active");
+        this.removeActive(item);
+      });
+    }
+  };
+}
